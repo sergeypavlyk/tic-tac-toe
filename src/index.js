bdfaaad636afd2ba19cab5f9a741f9d8b9f58224
sergeypavlyk <pavlyk.sergeynik@gmail.com>
2019-11-03 17:17:29 +0200

    main logic added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// styles for game
 document.body.style = "background-image: url(iphone.png); background-repeat: no-repeat; background-size: 500px; background-position: top; padding-top: 150px";
 const gameArea = document.querySelector('#root');
 gameArea.style = "width: 309.6px; height: 308px; background-color: blue; box-sizing: border-box; margin: 0 auto; border-radius: 10px";
@@ -10,16 +11,23 @@ restart.style = "padding: 20px; border: 3px solid white; margin: 0 auto; color: 
 restart.innerHTML = "Restart Game";
 restart.addEventListener('mouseenter', () => restart.style = "padding: 20px; border: 3px solid white; margin: 0 auto; color: white; background-color: blue; font-size: 20px; display: block; cursor: pointer; border-radius: 10px");
 restart.addEventListener('mouseleave', () => restart.style = "padding: 20px; border: 3px solid white; margin: 0 auto; color: blue; font-size: 20px; display: block; cursor: pointer; border-radius: 10px");
+restart.addEventListener('click', () => restartGame());
+
+const inputClickAreas = () => {
+  for (let i = 0; i < 9; i++) {
+    const clickArea = document.createElement("div");
+    clickArea.style = "width: 100px; height: 100px; border: 2px solid #fff; float: left; cursor: pointer; color: black; font-size: 90px; text-align: center; vertical-align: middle";
+    gameArea.append(clickArea);
+    clickArea.addEventListener('mouseenter', () => clickArea.style = "background-color: red; width: 100px; height: 100px; border: 2px solid #fff; float: left; cursor: pointer; color: black; font-size: 90px; text-align: center; vertical-align: middle");
+    clickArea.addEventListener('mouseleave', () => clickArea.style = "width: 100px; height: 100px; border: 2px solid #fff; float: left; cursor: pointer; color: black; font-size: 90px; text-align: center; vertical-align: middle");
+    clickArea.setAttribute('class', 'item');
+    clickArea.setAttribute('id', `${i}`);
+  };
+}
+inputClickAreas();
 
 
-
-// game logic
-const clickAreas = document.querySelectorAll('.item');
-let gamer = 'X';
-let move = 0;
-let gamerText = `${gamer}'s move now`;
-changeGamerText.innerHTML = gamerText;
-
+//********GAME********//
 // combinations
 const winCombinations = [
   [1, 2, 3],
@@ -34,20 +42,17 @@ const winCombinations = [
 let inputsX = [];
 let inputsO = [];
 
+// logic
+const clickAreas = document.querySelectorAll('.item');
+let gamer = 'X';
+let move = 0;
+let gamerText = `${gamer}'s move now`;
+changeGamerText.innerHTML = gamerText;
 
-const inputClickAreas = () => {
-  for (let i = 0; i < 9; i++) {
-    const clickArea = document.createElement("div");
-    clickArea.style = "width: 100px; height: 100px; border: 2px solid #fff; float: left; cursor: pointer; color: black; font-size: 90px; text-align: center; vertical-align: middle";
-    gameArea.append(clickArea);
-    clickArea.addEventListener('mouseenter', () => clickArea.style = "background-color: red; width: 100px; height: 100px; border: 2px solid #fff; float: left; cursor: pointer; color: black; font-size: 90px; text-align: center; vertical-align: middle");
-    clickArea.addEventListener('mouseleave', () => clickArea.style = "width: 100px; height: 100px; border: 2px solid #fff; float: left; cursor: pointer; color: black; font-size: 90px; text-align: center; vertical-align: middle");
-    clickArea.setAttribute('class', 'item');
-    clickArea.setAttribute('id', `${i}`);
-  };
-}
 
-for (let i = 0; i < clickAreas.length; i++) clickAreas[i].addEventListener('click', (e) => {
+for (let i = 0; i < clickAreas.length; i++) clickAreas[i].addEventListener('click', e => inputValue(e));
+
+const inputValue = e => {
   let num = e.target.id;
 
   if (e.target.innerHTML === '') {
@@ -57,6 +62,8 @@ for (let i = 0; i < clickAreas.length; i++) clickAreas[i].addEventListener('clic
 
   // push to new arrays
   (e.target.innerHTML === 'X') ? inputsX.push(num) : inputsO.push(num);
+  console.log(inputsX);
+  console.log(inputsO);
   if (
     (inputsX.length > 2 || inputsO.length > 2) &&
     (checkWinner(inputsX, num) || checkWinner(inputsO, num))
@@ -68,24 +75,21 @@ for (let i = 0; i < clickAreas.length; i++) clickAreas[i].addEventListener('clic
   }
 
   if (move === 9) {
-    let yes = alert('no winner!');
-    if (yes === true) restartGame();
+    alert('no winner!');
+    restartGame();
   }
   else if (move % 2 !== 0) {
     gamer = 'O'
   } else gamer = 'X';
 
   changeText();
-});
-
-
+}
 
 
 const changeText = () => {
   gamerText = `${gamer}'s move now`;
   changeGamerText.innerHTML = gamerText;
 }
-
 
 const checkWinner = (array, number) => {
   for (let w = 0, wLen = winCombinations.length; w < wLen; w++) {
@@ -119,10 +123,3 @@ const restartGame = () => {
 }
 
 
-const restartGameByButton = () => restart.addEventListener('click', () => restartGame());
-
-
-inputClickAreas();
-checkWinner();
-restartGameByButton();
-restartGame();
